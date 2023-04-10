664618fe78f03ea713e34c3963cf8b7f366b222c
OleksandrPinchuk <hohhua@gmail.com>
2023-04-10 18:11:41 +0200

    did the first part of the task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector(`.gallery`);
 
-console.log(galleryItems);
+const galleryMarkup = createGallery(galleryItems);
+
+function createGallery(item) {
+return item.map(({ preview, original, description }) => {
+    return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </li>`;
+    }).join('');
+};
+
+galleryEl.insertAdjacentHTML(`beforeend`, galleryMarkup);
+
+galleryEl.addEventListener(`click`, onGalleryItemClick);
+
+
+function onGalleryItemClick(event) {
+    event.preventDefault();
+    const currentItem = event.target;
+
+    const instance = basicLightbox.create(
+        `<div class="modal-image">
+            <img class="gallery__link" src="${currentItem.dataset.source}" width="800" height="600">
+        </div>`);
+    
+    if (currentItem.nodeName !== `IMG`) {
+        return
+    };
+    instance.show();
+    
+    window.addEventListener(`keydown`, handleModalClose);
+
+    function handleModalClose(event) {
+        if (event.code === "Escape")
+        {
+            instance.close()
+        };
+    };   
+};
+
